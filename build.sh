fa83703f0cbc422c855797b32eaf3fd6d1b8fe64
Nathanael <timmynathan10@gmail.com>
2022-10-04 16:50:43 +0700

    adding vcpkg

--- build.sh
+++ build.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+echo "---- making build directory ----"
 mkdir build
+
+echo "---- change work dir to build ----"
 cd build
-cmake ..
-make
+
+echo "---- Check if ninja build is available ----"
+
+if ! command -v ninja &> /dev/null; then
+   cmake .. -DCMAKE_BUILD_TYPE=Release
+else
+   cmake .. -DCMAKE_BUILD_TYPE=Release -GNinja
+fi
+
+echo "---- Building ----"
+
+if ! command -v ninja &> /dev/null; then
+   make
+else
+   ninja
+fi
+
+echo "---- Going back to source dir ----"
+cd ..
