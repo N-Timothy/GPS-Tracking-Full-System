530735460252f7b20d5a5a8c577a726f330b18f8
Nathanael <timmynathan10@gmail.com>
2022-10-06 13:18:44 +0700

    adding lock

--- src/GPS-Tracking/server/tcp.cpp
+++ src/GPS-Tracking/server/tcp.cpp
@@ -145,7 +145,7 @@ namespace karlo {
 
         // If something happened on the master socket, then it's an incoming connection
 
-        if (FD_ISSET(master_socket, &readfds) && lock) {
+        if (FD_ISSET(master_socket, &readfds)) {
           // If failed to accept connection
           if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
             perror("Accept");
@@ -154,7 +154,7 @@ namespace karlo {
 
             lock = false;
 
-          if (std::find(threads.begin(), threads.end(), new_socket) == threads.end()) {
+          if (std::find(threads.begin(), threads.end(), new_socket) == threads.end() && lock) {
               threads.push_back(new_socket);
               for(auto thread : threads){
                   std::cout << " | " << thread;
