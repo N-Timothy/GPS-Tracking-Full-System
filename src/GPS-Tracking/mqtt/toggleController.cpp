4a87d3006bcfa1bb9c96fb4810e1836b123129aa
Nathanael <timmynathan10@gmail.com>
2022-10-28 17:04:29 +0700

    switching mqtt to paho-mqtt

--- src/GPS-Tracking/mqtt/toggleController.cpp
+++ src/GPS-Tracking/mqtt/toggleController.cpp
@@ -132,6 +132,7 @@ namespace karlo {
                 }
                 std::cout << "Toggle False by : " << driverId << std::endl;
                 //int res = publisher(imei);
+                int res = -1;
                 std::cout << std::endl;
 
                 if (res == -1) {
