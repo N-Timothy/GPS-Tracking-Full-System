c583fca1cd514aa3f339b7abe9f662e61c9f8cb7
Nathanael <timmynathan10@gmail.com>
2022-11-08 09:59:15 +0700

    adding-status

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-//#include "GPS-Tracking/core/multiThread.hpp"
-#include "GPS-Tracking/server/tcp.hpp"
+#include "GPS-Tracking/core/multiThread.hpp"
+//#include "GPS-Tracking/server/tcp.hpp"
 
 #include <signal.h>
 
 int main(int argc, char *argv[]){
 
-//    signal(SIGPIPE, SIG_IGN);
+    signal(SIGPIPE, SIG_IGN);
 
-//    karlo::core::multiThread();
-    karlo::server::tcpServer();
+    karlo::core::multiThread();
+//    karlo::server::tcpServer();
 
   return 0;
 }
