509842b3b8e495595bf5e764025fe2a59f171c75
Nathanael <timmynathan10@gmail.com>
2023-02-16 13:43:40 +0700

    Testing new gps

--- src/GPS-Tracking/server/server_R11.cpp
+++ src/GPS-Tracking/server/server_R11.cpp
@@ -57,43 +57,43 @@ namespace karlo {
           std::stringstream ss;
 
           unsigned int start_bit;
-          ss << std::setw(2) << std::setfill('0') << std::hex << rawData.substr(0, PACKET_LENGTH * 2);
+          ss << std::hex << rawData.substr(0, PACKET_LENGTH * 2);
           ss >> start_bit;
 
           std::cout << "start bit : " << ss.str() << std::endl; 
 
           unsigned int packet_length;
-          ss << std::setw(2) << std::setfill('0') << std::hex << rawData.substr(4, PACKET_LENGTH * 2);
+          ss << std::hex << rawData.substr(4, PACKET_LENGTH * 2);
           ss >> packet_length;
 
           std::cout << "packet length : " << ss.str() << std::endl; 
 
           unsigned int protocol_number;
-          ss << std::setw(2) << std::setfill('0') << std::hex << rawData.substr(6, PROTOCOL_NUMBER * 2);
+          ss << std::hex << rawData.substr(6, PROTOCOL_NUMBER * 2);
           ss >> protocol_number;
 
           std::cout << "protocol number : " << ss.str() << std::endl; 
 
           unsigned int imei;
-          ss << std::setw(2) << std::setfill('0') << std::hex << rawData.substr(8, IMEI * 2);
+          ss << std::hex << rawData.substr(8, IMEI * 2);
           ss >> imei;
 
           std::cout << "imei : " << ss.str() << std::endl; 
 
           unsigned int serial_number;
-          ss << std::setw(2) << std::setfill('0') << std::hex << rawData.substr(24, SERIAL_NUMBER * 2);
+          ss << std::hex << rawData.substr(24, SERIAL_NUMBER * 2);
           ss >> serial_number;
 
           std::cout << "serial number : " << ss.str() << std::endl; 
           
           unsigned int error_check;
-          ss << std::setw(2) << std::setfill('0') << std::hex << rawData.substr(28, ERROR_CHECK * 2);
+          ss << std::hex << rawData.substr(28, ERROR_CHECK * 2);
           ss >> error_check;
 
           std::cout << "error check : " << ss.str() << std::endl; 
 
           unsigned int stop_bit;
-          ss << std::setw(2) << std::setfill('0') << std::hex << rawData.substr(32, STOP_BIT * 2);
+          ss << std::hex << rawData.substr(32, STOP_BIT * 2);
           ss >> stop_bit;
 
           std::cout << "stop bit : " << ss.str() << std::endl; 
