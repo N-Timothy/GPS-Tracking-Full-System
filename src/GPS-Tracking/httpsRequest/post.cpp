13b78a27aaa56eb25a90181f9bb89244dc6079d0
Nathanael <timmynathan10@gmail.com>
2024-06-12 09:05:38 +0700

    fix battStatus

--- src/GPS-Tracking/httpsRequest/post.cpp
+++ src/GPS-Tracking/httpsRequest/post.cpp
@@ -191,14 +191,14 @@ void post(std::string URL, json config, json data) {
   //   std::cout << "staging: " << data["imei"] << std::endl;
   // }
 
-  if (res_gps_backend) {
-    std::cout << "BODY" << MsgBackend << std::endl;
-    std::cout << "----" << std::endl;
-    std::cout << "gps_backend: " << res_gps_backend->body << std::endl;
-    std::cout << "------------------------------------------------------"
-              << std::endl;
-    // std::cout << "gps_backend: " << data["imei"] << std::endl;
-  }
+  // if (res_gps_backend) {
+  //   std::cout << "BODY" << MsgBackend << std::endl;
+  //   std::cout << "----" << std::endl;
+  //   std::cout << "gps_backend: " << res_gps_backend->body << std::endl;
+  //   std::cout << "------------------------------------------------------"
+  //             << std::endl;
+  //   // std::cout << "gps_backend: " << data["imei"] << std::endl;
+  // }
 }
 
 } // namespace httpsRequest
