5f9091c699054ec78e2a05315ffa8f2bdd9bedb3
Nathanael <timmynathan10@gmail.com>
2024-08-03 16:53:43 +0700

    timeout

--- src/GPS-Tracking/httpsRequest/post.cpp
+++ src/GPS-Tracking/httpsRequest/post.cpp
@@ -173,10 +173,22 @@ void post(std::string URL, json config, json data) {
 
   // auto res = cli.Post(postUrl, Msg, "application/json");
   // auto res_staging = cli_staging.Post(postUrl, Msg, "application/json");
-  auto res_gps_backend = cli_gps_backend.Post("/gps/last-location", MsgBackend,
-                                              "application/json");
-
   std::string imei = to_string(data["imei"]);
+
+  if (stoi(imei) % 2 == 0) {
+
+    auto res_gps_backend1 = cli_gps_backend.Post(
+        "/gps/last-location1", MsgBackend, "application/json");
+
+    std::cout << "gps_backend 1 : " << res_gps_backend1 << std::endl;
+
+  } else {
+
+    auto res_gps_backend2 = cli_gps_backend.Post(
+        "/gps/last-location2", MsgBackend, "application/json");
+
+    std::cout << "gps_backend 2 : " << res_gps_backend2 << std::endl;
+  }
 
   // auto res_beta = cli_beta.Post(postUrl, Msg, "application/json");
   // if (res_beta) {
