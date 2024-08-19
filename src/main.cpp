11d5962053200a6baf92e5bf0f57cd854d99468f
Nathanael <timmynathan10@gmail.com>
2024-08-19 16:42:54 +0700

    testing axios

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,16 @@
 
 int main(int argc, char *argv[]) {
 
-  //    signal(SIGPIPE, SIG_IGN);
+  signal(SIGPIPE, SIG_IGN);
 
-  //  karlo::core::multiThread();
-  try {
-    boost::asio::io_context io_context;
-    karlo::tcp::Server server(io_context, 8080, 100);
-    io_context.run();
-  } catch (std::exception &e) {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
+  karlo::core::multiThread();
+  // try {
+  //   boost::asio::io_context io_context;
+  //   karlo::tcp::Server server(io_context, 8080, 100);
+  //   io_context.run();
+  // } catch (std::exception &e) {
+  //   std::cerr << "Exception: " << e.what() << "\n";
+  // }
 
   return 0;
 }
