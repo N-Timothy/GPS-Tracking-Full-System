a86f5b869dc224dda0a822acdd08bc6d9d09e3df
Nathanael <timmynathan10@gmail.com>
2024-08-19 15:24:12 +0700

    testing axios

--- src/GPS-Tracking/server/server.cpp
+++ src/GPS-Tracking/server/server.cpp
@@ -546,7 +546,7 @@ int communicate(int connfd) {
       // for (auto postDataIt : postDataVec) {
       // postDataCount++;
       // std::cout << "POSTING DATA " << postDataCount << "\n";
-      std::cout << "hex stream" << hex_stream << std::endl;
+      std::cout << "hex stream :   " << hex_stream << std::endl;
       try {
         httpsRequest::singleConnect(postData);
         //        mqtt::publisher(data.imei);
